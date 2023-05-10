6d6f18cdf282534ebd5b7952c149a974077a3c2a
Abdillah Naufal Lesmana <naufaldec07@gmail.com>
2023-05-10 11:23:44 +0700

    p

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,12 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticationController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware(['auth:sanctum'])->only('logout', 'my');
+    }
     public function login(Request $request)
     {
-        
+        $request->validate([
+            'email' => 'email|required',
+            'password' => 'required',
+        ]);
+        $user = User::where('email', $request->email)->first();
+ 
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+
+    }
+    return $user->createToken($user->username)->plainTextToken;
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Anda Telah Logout']);
+    }
+
+    public function my()
+    {
+        $user = Auth::user();
+        return response()->json([
+            'id' => $user->id,
+            'username' => $user->username,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,6 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
 Route::get('/posts/2/{id}', [PostController::class, 'show2']);
 
-Route::post('login', [AuthenticationController::class, 'login']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::get('/logout', [AuthenticationController::class, 'logout']);
+Route::get('/my', [AuthenticationController::class, 'my']);
